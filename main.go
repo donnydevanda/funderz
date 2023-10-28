99b6a39b8de70a23e85a38fa1d1f78301b68406f
donnydevanda <devandadonny@gmail.com>
2023-10-28 09:16:23 +0700

    User services init

--- main.go
+++ main.go
@@ -1,20 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"funderz/handler"
+	"funderz/user"
 	"log"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
 	dsn := "root:@tcp(127.0.0.1:3306)/funderz?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	fmt.Println("Connection to DB OK")
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+	
+	userHandler := handler.NewUserHandler(userService)
+
+	router := gin.Default()
+
+	api := router.Group("/api/v1")
+
+	api.POST("/users", userHandler.RegisterUser)
+
+	router.Run()
 }
