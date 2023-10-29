735598f04f73d9ec63c066281da7260335e6cfb9
donnydevanda <devandadonny@gmail.com>
2023-10-29 18:29:48 +0700

    User JSON response

--- handler/user.go
+++ handler/user.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"funderz/helper"
 	"funderz/user"
 	"net/http"
 
@@ -23,10 +24,15 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, nil)
 	}
 
-	user, err := h.userService.RegisterUser(input)
+	newUser, err := h.userService.RegisterUser(input)
+
 	if err != nil {
 		c.JSON(http.StatusBadRequest, nil)
 	}
 
-	c.JSON(http.StatusOK, user)
+	formatter := user.FormatUser(newUser, "tokentokentokentoken")
+
+	response := helper.ApiResponse("Account has been registered", http.StatusOK, "success", formatter)
+
+	c.JSON(http.StatusOK, response)
 }
